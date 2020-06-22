28f1764fafa539be521d1fa8c5b00030caf3d3c0
lukelin1991 <lukelin1991@yahoo.com>
2020-06-22 16:49:09 -0400

    adding components

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from './components/Header';
+import { uuid } from 'uuidv4';
+
 const App = () => {
+  const [items, setItems] = useState([
+    { id: uuid(), text: 'Milk' },
+    { id: uuid(), text: 'Eggs' },
+    { id: uuid(), text: 'Bread' },
+    { id: uuid(), text: 'Juice' }
+  ])
+
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} style={styles.img} />
+      <Header />
     </View>
   )
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
+    flex: 1,
+    paddingTop: 60,
   },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
-  }
 })
 
 export default App;
